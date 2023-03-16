5e2f777b51a137391e3103e93db60e65de39c334
Kunal8879 <kunalsaini8879@gmail.com>
2023-03-16 14:30:37 +0530

    added time pop up on message hover

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -10,6 +10,7 @@ export class ChatbotComponent implements OnInit{
 
   messages: Message[] = [];
   value: string = '';
+  isLoading: boolean = true;
 
   constructor(private chatbotServiceService: ChatbotServiceService) { }
 
@@ -18,18 +19,20 @@ export class ChatbotComponent implements OnInit{
       this.messages = this.messages.concat(val);
     });
     this.messages.push(new Message('bot', 'Hello, I am Test Bot. How can I help you?', new Date().toString().split(' ')[4]));
+    this.isLoading = false;
   }
 
   sendMessage() {
+
     if(this.value.trim() != '') {
+      this.isLoading = true;
+      console.log(this.isLoading);
       this.chatbotServiceService.getBotAnswer(this.value);
       this.value = '';
+      setTimeout(() => { this.isLoading = false; }, 1500);
     }
+    console.log(this.isLoading);
+    
   }
-
-  // getResponse() {
-  //   console.log(this.userMessage);
-  //   this.userPreviousMessage.push(this.userMessage);
-  // }
 
 }
