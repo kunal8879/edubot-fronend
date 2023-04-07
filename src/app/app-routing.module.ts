a09b1eaa8c984d311dc7eb2aaffd140fc71b0762
GaurangSant <gaurang.sant612@gmail.com>
2023-04-07 10:42:15 +0530

    Code Optimized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,27 +7,12 @@ import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './menu/login/login.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/user-list', component: UserListComponent },
   { path: 'login', component: LoginComponent},
-  // {path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'menu', component: MenuComponent},
-  // {path: '', redirectTo: '/menu', pathMatch: 'full'},
   { path: 'user-list', component: UserListComponent },
-  // {path: '', redirectTo: '/user-list', pathMatch: 'full'},
   { path: 'unanswered-questions', component: UnansweredQuestionsComponent},
-  // {path: '', redirectTo: '/unanswered-questions', pathMatch: 'full'},
-  // { path: '', redirectTo: '/users', pathMatch: 'full' }
-
-
-  // { path: '', redirectTo: '/', component: MoodleComponent, pathMatch: 'full' }
-
-  // { path: 'users', component: UserListComponent },
-  // { path: 'unanswered-questions', component: UnansweredQuestionsComponent },
-  // { path: '', pathMatch: 'full', component: MoodleComponent },
   { path: '**', pathMatch: 'full', component: MoodleComponent },
   { path: 'menu', pathMatch: 'full', component: LoginComponent },
-  // { path: '', redirectTo: '/menu', pathMatch: 'full' },
-
 ];
 
 @NgModule({
