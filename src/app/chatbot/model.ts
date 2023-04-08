c94600da7ab2dc50a5175222bd14ceeab88502ab
Kunal8879 <kunalsaini8879@gmail.com>
2023-04-08 23:53:58 +0530

    frontend tweeks

--- src/app/chatbot/model.ts
+++ src/app/chatbot/model.ts
@@ -1,15 +1,11 @@
 export class MessageModel {
     author: string;
-    sentence: string;
+    chat: string;
     time: string;
 
     constructor(author: string, sentence: string, time: string) {
         this.author = author;
-        this.sentence = sentence;
+        this.chat = sentence;
         this.time = time;
     }
 }
-
-// export class MessageModel {
-//     sentence: string;
-// }
