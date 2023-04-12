3119028761452012217f02e628b8f2b4577fdd03
Kunal8879 <kunalsaini8879@gmail.com>
2023-04-12 23:46:51 +0530

    login page

--- src/app/menu/login/login.component.ts
+++ src/app/menu/login/login.component.ts
@@ -8,7 +8,7 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+  hide : boolean = true;
 
   ngOnInit() {
   }
